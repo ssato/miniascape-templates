68752cfc65ae42d65237c31d3e4d769d69701f40
Satoru SATOH <satoru.satoh@gmail.com>
2019-01-29 00:51:13 +0900

    enhancement: [rhui] build client cert and client config rpm at a time if it can

--- autoinstall.d/data/rhui/3/rhua/90_setup_rhui_for_clients.sh
+++ autoinstall.d/data/rhui/3/rhua/90_setup_rhui_for_clients.sh
@@ -23,18 +23,29 @@ test -f ~/.rpmmacros || bash -x ${0%/*}/gen_rpm_gpgkey.sh
 # List repos available to clients
 rhui-manager ${RHUI_AUTH_OPT} client labels
 
+can_build_cert_and_rpm_at_a_time=$(rhui-manager client rpm | grep '\-\-cert ' >/dev/null 2>/dev/null && echo T || echo F)
+
 mkdir -p ${RHUI_CLIENT_WORKDIR:?}
 while read line
 do
     test "x$line" = "x" && continue || :
-    name=${line%% *}; repos=${line#* }
+    name=${line%% *}; repos=${line#* };
+    [[ "$can_build_cert_and_rpm_at_a_time" = "T" ]] && {
+        version=1.0;
+        rhui-manager ${RHUI_AUTH_OPT} client rpm \
+            --cert --rpm_name ${name:?} --rpm_version ${version:?} \
+            --repo_label ${repos:?} \
+            --days 3651 --dir ${RHUI_CLIENT_WORKDIR}/
+    } || {
     rhui-manager ${RHUI_AUTH_OPT} client cert \
         --name ${name:?} --repo_label ${repos:?} \
         --days 3651 --dir ${RHUI_CLIENT_WORKDIR}/
+    }
 done << EOC
 ${RHUI_CLIENT_CERTS:?}
 EOC
 
+[[ "$can_build_cert_and_rpm_at_a_time" = "T" ]] || {
 mkdir -p ${RHUI_CLIENT_RPMS_DIR}
 while read line
 do
@@ -48,6 +59,7 @@ do
 done << EOC
 ${RHUI_CLIENT_RPMS:?}
 EOC
+}
 
 # Check
 find ${RHUI_CLIENT_WORKDIR}/ -type f
